5d3baba0be9343197adb09ed231e21ba1c0fe1f3
Winston Parris <anthonyparris16@gmail.com>
2018-02-24 16:41:48 -0500

    time implentation

--- app/src/main/java/com/example/gracevictoria/atlhack2018/Clock.java
+++ app/src/main/java/com/example/gracevictoria/atlhack2018/Clock.java
@@ -4,6 +4,8 @@ import java.sql.Time;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Created by Grace Victoria on 2/24/2018.
@@ -14,10 +16,10 @@ public class Clock {
 
 
 
-    public static void outputTime() {
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
-        LocalDateTime now = LocalDateTime.now();
-        System.out.println(dtf.format(now));
+
+    public Date void outputTime() {
+    Date currentTime = Calendar.getInstance().getTime();
+       return currentTime;
     }
 
 
