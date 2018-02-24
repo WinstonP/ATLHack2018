87cb091a95044970d35fd63785f4ac4cf08290eb
Winston Parris <anthonyparris16@gmail.com>
2018-02-24 16:36:53 -0500

    Added the time function

--- app/src/main/java/com/example/gracevictoria/atlhack2018/Clock.java
+++ app/src/main/java/com/example/gracevictoria/atlhack2018/Clock.java
@@ -1,6 +1,9 @@
 package com.example.gracevictoria.atlhack2018;
 
+import java.sql.Time;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  * Created by Grace Victoria on 2/24/2018.
@@ -8,5 +11,17 @@ import java.time.LocalTime;
 
 public class Clock {
     private LocalTime localTime;
+
+
+
+    public static void outputTime() {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println(dtf.format(now));
+    }
+
+
+    //////////////////////////////
+
 
 }
