575f24910e97dbd0e89d1b38e55ad53e6db494ff
Ryan Hunter <rhunter4@xula.edu>
2018-02-24 14:00:06 -0600

    hola

--- app/src/main/java/com/example/gracevictoria/atlhack2018/MainActivity.java
+++ app/src/main/java/com/example/gracevictoria/atlhack2018/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.w("Greetings!", "Welcome to our app!");
         Log.w("Yo","This is Winston");
-        Log.w("Hellllooooo this is ryan <3")
+        Log.w("Hellllooooo this is ryan <3", "yo");
     }
 
 }
