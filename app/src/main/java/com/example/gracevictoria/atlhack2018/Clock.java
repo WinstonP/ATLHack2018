007992071f00854e14de4ad524dbcb98e81ac0bf
Grace Braden <gracebraden97@gmail.com>
2018-02-24 16:08:12 -0500

    Added Clock class

--- app/src/main/java/com/example/gracevictoria/atlhack2018/Clock.java
+++ app/src/main/java/com/example/gracevictoria/atlhack2018/Clock.java
@@ -1,8 +1,12 @@
 package com.example.gracevictoria.atlhack2018;
 
+import java.time.LocalTime;
+
 /**
  * Created by Grace Victoria on 2/24/2018.
  */
 
 public class Clock {
+    private LocalTime localTime;
+
 }
